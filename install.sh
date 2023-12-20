3746b35a4d83d9ab98a00bb7ee1bf138f4e0790b
LatekVon <latkaignacy@gmail.com>
2023-12-21 00:15:26 +0100

    added multi-platform support for install.sh, fixed issues found on mac systems

--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+# create a directory for the project
 mkdir h2o_chat
 cd h2o_chat
 
-pip install virtualenv
-python -m venv h2o_venv
-source h2o_venv/bin/activate
+# install virtualenv
+pip3 install virtualenv
 
-# Windows CUDA / Linux CUDA:
-export PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cu118"
-# CPU ONLY / M1 / M2:
-#export PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cpu"
+# create a virtual environment
+python3 -m venv h2o_venv
+
+# activate the virtual environment (platform-independent)
+source h2o_venv/bin/activate || source h2o_venv/Scripts/activate
+
+# set environment variable for CUDA based on the OS
+if [ "$(uname)" == "Darwin" ]; then
+    export PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cpu"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    set PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cu118"
+else
+    export PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cu118"
+fi
+
+# clone the repository
 git clone https://github.com/h2oai/h2ogpt.git
 cd h2ogpt
 
-# No cache fixes a lot of install issues that i encountered
-pip install --no-cache-dir -r requirements.txt
-pip install --no-cache-dir -r reqs_optional/requirements_optional_langchain.txt
-pip install --no-cache-dir -r reqs_optional/requirements_optional_gpt4all.txt
-pip install --no-cache-dir -r reqs_optional/requirements_optional_langchain.urls.txt
-# Warning, GPL license:
-pip install -r reqs_optional/requirements_optional_langchain.gpllike.txt
+# install required packages
+pip3 install --no-cache-dir -r requirements.txt
+pip3 install --no-cache-dir -r reqs_optional/requirements_optional_langchain.txt
+pip3 install --no-cache-dir -r reqs_optional/requirements_optional_gpt4all.txt
+pip3 install --no-cache-dir -r reqs_optional/requirements_optional_langchain.urls.txt
+# warning: GPL license:
+pip3 install -r reqs_optional/requirements_optional_langchain.gpllike.txt
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python h2o_chat/h2ogpt/generate.py --base_model=TheBloke/zephyr-7B-beta-GGUF --prompt_type=zephyr --max_seq_len=4096
+python3 h2o_chat/h2ogpt/generate.py --base_model=TheBloke/zephyr-7B-beta-GGUF --prompt_type=zephyr --max_seq_len=4096
