ea8d0f5d5bd2a9ed2347855a53cf35777459aa2b
LatekVon <latkaignacy@gmail.com>
2023-12-21 02:40:58 +0100

    fixed all bugs, llama model launches successfully

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ python3 -m venv h2o_venv
 # activate the virtual environment (platform-independent)
 source h2o_venv/bin/activate || source h2o_venv/Scripts/activate
 
-# set environment variable for CUDA based on the OS
+# set extra repositories for pip, OS dependant
 if [ "$(uname)" == "Darwin" ]; then
     export PIP_EXTRA_INDEX_URL="https://download.pytorch.org/whl/cpu"
 elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
@@ -27,9 +27,15 @@ git clone https://github.com/h2oai/h2ogpt.git
 cd h2ogpt
 
 # install required packages
+pip3 install --upgrade pip
+pip3 install llama-cpp-python
+pip3 install librosa soundfile wavio DocTR LangChain
 pip3 install --no-cache-dir -r requirements.txt
 pip3 install --no-cache-dir -r reqs_optional/requirements_optional_langchain.txt
 pip3 install --no-cache-dir -r reqs_optional/requirements_optional_gpt4all.txt
 pip3 install --no-cache-dir -r reqs_optional/requirements_optional_langchain.urls.txt
 # warning: GPL license:
 pip3 install -r reqs_optional/requirements_optional_langchain.gpllike.txt
+
+# generate and run the model
+python3 generate.py --base_model=TheBloke/zephyr-7B-beta-GGUF --prompt_type=zephyr --max_seq_len=4096
